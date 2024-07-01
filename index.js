a86c5c5453729d985587cdf61d02291c9765a36d
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-01 10:08:12 +0300

    lab.md

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide = (x, y) => {
+    console.log (`Dividing ${x}`);
+    console.log (`Dividing ${y}`);
+    return x / y;
+}
+
+divide(200,100)
+
+
+let x = 2
+const square = x => x * x 
+
+const add = (a, b) => {
+    console.log (`Adding ${a}`);
+    console.log (`Adding ${b}`)
+    return a + b;
+
+}
+add(3, 4)
